7c8e245a2b16381edfd295ab5c5c682372fda507
ndri45 <asndr82@gmail.com>
2022-09-24 14:44:26 +0700

    modified contact app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import './App.css';
+import { Grid } from "@mui/material";
+import "./App.css";
+import Header from "./components/Header";
+import ContactForm from "./components/ContactForm";
+import Contact from "./components/Contact";
+import db from "./data/contacts.json";
+import { useState } from "react";
 
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
 const App = () => {
-  // Masukkan Header dan lakukan map untuk Contact ke dalam div App
-  // untuk membuat daftar kontak bisa menggunakan MUI list
-  // https://mui.com/material-ui/react-list/#folder-list
+    // Masukkan Header dan lakukan map untuk Contact ke dalam div App
+    // untuk membuat daftar kontak bisa menggunakan MUI list
+    // https://mui.com/material-ui/react-list/#folder-list
 
-  // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
-  // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+    // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
+    const [contacts, setContacts] = useState(db);
+    // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+    const addContact = (value) => {
+        setContacts([...contacts, value]);
+    };
 
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <ContactForm addContact={addContact} />
+                </Grid>
+                <Grid item xs={6}>
+                    <Contact data={contacts} />
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default App;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,13 +3,38 @@
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = () => {
+import { Button, Card, TextField } from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
+
+const ContactForm = ({ addContact }) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
+    const [newContact, setNewContacts] = useState({});
 
     return (
-        <> </>
+        <>
+            <Card sx={{ margin: "80px", padding: "20px" }}>
+                <Stack spacing={2}>
+                    <TextField required fullWidth id="filled-required" name="name" label="Name" variant="filled" onChange={(event) => setNewContacts({ ...newContact, name: event.target.value })} />
+                    <TextField required fullWidth id="filled-required" name="phone" label="Phone" variant="filled" onChange={(event) => setNewContacts({ ...newContact, phone: event.target.value })} />
+                    <TextField required fullWidth id="filled-required" name="email" label="Email" variant="filled" onChange={(event) => setNewContacts({ ...newContact, email: event.target.value })} />
+                    <TextField required fullWidth id="filled-required" name="url" label="URL" variant="filled" onChange={(event) => setNewContacts({ ...newContact, url: event.target.value })} />
+                    <Button
+                        sx={{ width: "fit-content" }}
+                        color="primary"
+                        variant="text"
+                        onClick={() => {
+                            addContact(newContact);
+                            document.querySelectorAll("input").forEach((input) => (input.value = ""));
+                        }}
+                    >
+                        Add New
+                    </Button>
+                </Stack>
+            </Card>
+        </>
     );
-}
+};
 
 export default ContactForm;
